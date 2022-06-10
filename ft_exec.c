b88d5a7d2775998bef1dc08bba328f84917ac7fb
Lachelle Caitlyn <lcaitlyn@un-q4.kzn.21-school.ru>
2022-06-10 10:38:56 +0300

    добавил чтобы работала команда если прилетает абсолютный путь

--- ft_exec.c
+++ ft_exec.c
@@ -71,11 +71,13 @@ char	**ft_find_paths(char *envp[])
 void	ft_exec(char *path, char *argv, char *envp[])
 {
 	char	**cmd;
-
+	
 	if (ft_strlen(argv) == 0)
 		ft_perror("");
 	cmd = ft_split(argv, ' ');
-	if (!path)
+	if (access(cmd[0], X_OK) == 0)
+		path = cmd[0];
+	if (!path || access(path, X_OK) == -1)
 		path = ft_find_cmd(cmd[0], ft_find_paths(envp));
 	if (!path)
 		ft_perror ("execve");
